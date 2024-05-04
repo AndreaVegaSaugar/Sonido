2b4358f17a5520bd390fb1803eddd51214295b95
DavidRainder <100291375+ClaudiaZarzuela@users.noreply.github.com>
2024-05-04 18:15:37 +0200

    TECLADOOOoOoOo
    Co-Authored-By: Paula López <92334964+f0reil@users.noreply.github.com>

---
+++ Assets/showKeyBoard.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Microsoft.MixedReality.Toolkit.Experimental.UI;
+public class showKeyBoard : MonoBehaviour
+{
+    private TMP_InputField inputField;
+
+    public float distance = 0.5f;
+    public float verticalOffset = -0.5f;
+
+    public Transform positionSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputField = GetComponent<TMP_InputField>();
+        inputField.onSelect.AddListener(x => OpenKeyBoard());
+    }
+
+    public void OpenKeyBoard()
+    {
+        NonNativeKeyboard.Instance.InputField = inputField;
+        NonNativeKeyboard.Instance.PresentKeyboard(inputField.text);
+
+        Vector3 dir = positionSource.forward;
+        dir.y = 0;
+        dir.Normalize();
+
+        Vector3 targetPos = positionSource.position + dir * distance + Vector3.up * verticalOffset;
+
+        NonNativeKeyboard.Instance.RepositionKeyboard(targetPos);
+
+        SetCaretColorAlpha(1);
+
+        NonNativeKeyboard.Instance.OnClosed += Instance_OnClosed;
+    }
+
+    private void Instance_OnClosed(object sender, System.EventArgs e)
+    {
+        SetCaretColorAlpha(0);
+        NonNativeKeyboard.Instance.OnClosed -= Instance_OnClosed;
+    }
+
+    public void SetCaretColorAlpha(float value)
+    {
+        inputField.customCaretColor = true;
+        Color caretColor = inputField.caretColor;
+        caretColor.a = value;
+        inputField.caretColor = caretColor;
+    }
+}
