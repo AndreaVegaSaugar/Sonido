d77985e1879fc0931f166926066c160c10a23800
Andrea Vega Saugar <anvega03@ucm.es>
2024-05-04 12:30:38 +0200

    cosa

---
+++ Assets/Scripts/Taquilla.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Taquilla : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "player")
+        {
+            Debug.Log("NOOOOOOO EL DALTONICO");
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
